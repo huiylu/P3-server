0d1a0461bdfab0342cbe6fcbb0ac801b66035041
EdJaPe <76016371+EdJaPe@users.noreply.github.com>
2021-02-19 09:09:36 -0800

    Initial commit

---
+++ controllers/auth.js
@@ -0,0 +1,30 @@
+const bcrypt = require('bcrypt');
+const express = require('express');
+const db = require('../models');
+const router = express.Router();
+
+
+// URL prefix — /api
+
+// Signup — POST /api/signup
+router.post('/signup', (req, res) => {
+  bcrypt.hash(req.body.password, 10)
+  .then(hash => db.User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: hash
+  }))
+  .then(createdUser => res.json(createdUser))
+  .catch(err => {
+    console.log(`🔥 Error in the POST signup:`, err);
+    res.json({ error: err})
+  });
+});
+
+// Login — POST /api/login
+router.post('/login', (req, res) => {
+  res.json({ message: 'LOGIN POST'});
+});
+
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const cors = require('cors');
+const express = require('express');
+
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Routes
+app.get('/', (req, res) => {
+  res.json({ message: "MERN app API Home"});
+});
+
+// Controllers
+app.use('/api', require('./controllers/auth'));
+
+app.listen(process.env.PORT || 3001, () => 
+  console.log(`🎧 You're listening to the smooth sounds of Port ${process.env.PORT || 3001}`)
+);
