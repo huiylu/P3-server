d8491a19dda640df0b0d6ec8b90e94db8a9903c1
damatoaj <mr.arthurdamato@gmail.com>
2021-02-20 14:08:30 -0800

    updated and pseudocoded

--- controllers/playlist.js
+++ controllers/playlist.js
@@ -3,34 +3,36 @@ const db = require('../models');
 const router = express.Router();
 
 //get route displays the playlists
-router.get('/playlist', (req, res) => {
-    res.json({ message: 'PLAYLIST POST'});
+router.get('/', (req, res) => {
+    res.json({ message: 'PLAYLIST GET'});
 });
 
 //post route allows you to create a new playlist
-router.post('/playlist', (req, res) => {
-
+router.post('/', (req, res) => {
+    res.json({ message: 'PLAYLIST POST'});
 });
 
 
 //get route that displays the individual playlist
-router.get('/playlist/:id', (req, res) => {
-
+router.get('/:id', (req, res) => {
+    res.json({ message: 'PLAYLIST ID POST'});
+    //find the user by id : findById()
+    
 });
 
 //put route that allows you to  update the individual playlists
-router.put('/playlist:id', (req, res) => {
-
+router.put('/:id', (req, res) => {
+    res.json({ message: 'PLAYLIST ID UPDATE'});
 });
 
 //get route that lets you edit the playlist (name etc)
-router.get('/playlist/:id', (req, res) => {
-
+router.get('/:id', (req, res) => {
+    res.json({ message: 'PLAYLIST ID EDIT'});
 });
 
 //delete rout lets you delete a playlist
-router.delete('/playlist/:id', (req, res) => {
-
+router.delete('/:id', (req, res) => {
+    res.json({ message: 'PLAYLIST ID DELETE'});
 })
 
 
--- controllers/profile.js
+++ controllers/profile.js
@@ -3,18 +3,21 @@ const db = require('../models');
 const router = express.Router();
 
 //get route that show the persons profile
-router.get('/profile/:id', (req, res) => {
+router.get('/:id', (req, res) => {
     res.json({ message: 'PROFILE POST'});
+    //find the user by id : findById()
+
+    
 });
 
 //put route that lets you update personal details
-router.put('/profile/:id', (req, res) => {
+router.put('/:id', (req, res) => {
 
 });
 
 
 //delete route in case you want to delete your profile
-router.delete('profile/:id', (req, res) => {
+router.delete('/:id', (req, res) => {
 
 });
 
