791ddcc7385ddd8ec9ad768b361970261b73df52
damatoaj <mr.arthurdamato@gmail.com>
2021-02-20 12:30:57 -0800

    stubbed out controller routes

---
+++ controllers/playlist.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const db = require('../models');
+const router = express.Router();
+
+//get route displays the playlists
+router.get('/playlist', (req, res) => {
+
+});
+
+//post route allows you to create a new playlist
+router.post('/playlist', (req, res) => {
+
+});
+
+
+//get route that displays the individual playlist
+router.get('/playlist/:id', (req, res) => {
+
+});
+
+//put route that allows you to  update the individual playlists
+router.put('/playlist:id', (req, res) => {
+
+});
+
+//get route that lets you edit the playlist (name etc)
+router.get('/playlist/:id', (req, res) => {
+
+});
+
+//delete rout lets you delete a playlist
+router.delete('/playlist/:id', (req, res) => {
+
+})
+
+
+module.exports = router;
---
+++ controllers/songs.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const db = require('../models');
+const router = express.Router();
+
+//get route to display the searched songs
+router.get('/songs', (req, res) => {
+
+});
+
+
+module.exports = router;
