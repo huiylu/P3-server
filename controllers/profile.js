a53ca24138f23caa55a304f2c70301c908ae682a
damatoaj <mr.arthurdamato@gmail.com>
2021-02-22 14:17:24 -0800

    update

--- controllers/profile.js
+++ controllers/profile.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const db = require('../models');
 const router = express.Router();
+const User = require('../models/user');
+const {findById} = require('../models/user');
 
 //get route that show the persons profile
-router.get('/:id', (req, res) => {
-    res.json({ message: 'PROFILE POST'});
+router.get('/:_id', (req, res) => {
+    // res.json({ message: 'PROFILE POST'});
     //find the user by id : findById()
-
+    User.findById(req.params.id, (err, user) => {
+        if(err) {
+            console.error(`Error in the GET ID for playlists\n${err}`);
+            res.status(500).json({error: `Error in the Detail ROUTE for playlists`});
+        }
+        res.json({user});
+    })
     
 });
 
@@ -18,7 +26,13 @@ router.put('/:id', (req, res) => {
 
 //delete route in case you want to delete your profile
 router.delete('/:id', (req, res) => {
-
+    User.findByIdAndDelete(req.params.id, (err, user) => {
+        if(err) {
+            console.error(`ERROR in the playlist DELETE ROUTE\n${err}`);
+            res.status(500).json({error: `ERROR in the playlists DELETE ROUTE`});
+        }
+        res.json({deletedUser: user})
+    })
 });
 
 module.exports = router;
--- controllers/songs.js
+++ controllers/songs.js
@@ -3,49 +3,56 @@ const db = require('../models');
 const router = express.Router();
 const request = require('request');
 const axios = require('axios');
+const FormData = require('form-data');
+const querystring = require('querystring');
 
 var client_id = process.env.CLIENT_ID; // Your client id
 var client_secret = process.env.CLIENT_SECRET; // Your secret
 
 // your application requests authorization
+ let data = new FormData();
+ data.append('grant_type', 'client_credentials');
+
 var authOptions = {
     method: 'post',
     url: 'https://accounts.spotify.com/api/token',
     headers: {
-      Authorization: 'Basic ' + (Buffer.from(client_id + ':' + client_secret).toString('base64')) //Basic <base64 encoded client_id:client_secret>
+      'Authorization': 'Basic ' + (Buffer.from(client_id + ':' + client_secret).toString('base64')), //Basic <base64 encoded client_id:client_secret>
+      'Content-Type': 'application/x-www-form-urlencoded'
     },
-    form: {
-      grant_type: 'client_credentials'
-    },
-    json: true
+    data: data
+    // data : {
+      // grant_type: 'client_credentials'
+    // },
+    // json: true
   };
 
 
 //get route to display the searched songs
 router.get('/', (req, res) => {
-    axios(authOptions)
-    .then(res => {
-      var token = res.access_token;
-      console.log(res.data)
+    axios.request(authOptions)
+    .then(response => {
+      var token = response.access_token;
+      console.log(response.access_token, '😡')
       var options = {
-                    //TODO use the form and string interpolation to get the artist and track (q, type, artist?)
-        
-        url: `https://api.spotify.com/v1/search?q=${req.body.data}&type=track%2Cartist&market=US&limit=10&offset=5`,
+        //TODO use the form and string interpolation to get the artist and track (q, type, artist?)
+        method: get,
+        url: `https://api.spotify.com/v1/search?q=${req.body.data||'muse'}&type=track%2Cartist&market=US&limit=10&offset=5`,
         headers: {
-            Authorization: 'Bearer ' + token,
+            'Authorization': 'Bearer ' + token,
             'Content-Type': 'application/x-www-form-urlencoded'
         },
-        transformRequest:  [(data, headers) => {
-          return data;
-        }]
-        };
-        axios.get(options)
+        // json: true
+      };
+      axios.request(options)
         .then (res => {
           console.log('@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@', (res.body));
+        }).catch(err => {
+          console.error(`IS THIS THE OTHER ERROR? \n${err}`)
         })
     }).catch(err => {
       if (err) {
-        console.error(`WE HAVE AN ERROR IN THE AXIOS CALL\n${err}`)
+        console.error(`WE HAVE AN ERROR IN THE AXIOS CALL`, err)
       }
     })
 
