255c5bcc2ac58b0d7a2491b18c20db412761d690
edJaPe <edv06jara@gmail.com>
2021-02-22 15:20:17 -0800

    solved auth issues

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,10 +1,10 @@
 require('dotenv').config();
 const passport = require('passport');
-const bcrypt = require('bcrypt');
 const Strategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
 const db = require('../models');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 
 // Construct the Strategy
 const options = {
@@ -21,27 +21,28 @@ const findUser = (jwt_payload, done) => {
 const strategy = new Strategy(options, findUser);
 
 // Register the strategy so passport uses it when we call `passport.authenticate()` in our routes
+passport.use(strategy);
 
 // initialize passport
-passport.use(strategy);
 passport.initialize();
 
+// write a function that creates a jwt token
 const createUserToken = (req, user) => {
+  // check the password from the req.body against the user
   const validPassword = bcrypt.compareSync(req.body.password, user.password);
-  if(!user ||!validPassword){
-      const err = new Error('Invalid')
-      err.statusCode=422;
-      throw err
-  } else {
-      const token = jwt.sign(
-          { id: user._id, email: user.email },
-          process.env.JWT_SECRET,
-          {expiresIn: '2m'}
-      )
-      return token;
+
+  // if we didn't get a user or the password isn't valid, then trow an error
+  if (!user || !validPassword) {
+    const err = new Error('Invalid Credentials 🛑');
+    err.statusCode = 422;
+    throw err
+  } else { // otherwise create and sign a new token
+    return jwt.sign(
+      { id: user._id, email: user.email },
+      process.env.JWT_SECRET,
+      { expiresIn: '20m'} // TODO: extend for production
+    );
   }
 }
 
-module.exports = {
-  createUserToken
-}
+module.exports = { createUserToken }
