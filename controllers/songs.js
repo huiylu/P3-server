b2efa292da619e2c0f1026be57740b7f91ecab95
damatoaj <mr.arthurdamato@gmail.com>
2021-02-25 14:05:58 -0800

    deployment

--- controllers/songs.js
+++ controllers/songs.js
@@ -50,8 +50,6 @@ router.get('/', (req, res) => {
   }}
   axios.get(`https://api.spotify.com/v1/search?q=${returnSearch}&type=track&market=US&limit=10&offset=5` , headers)
     .then (response => {
-      // console.log('🦀', (response.query));
-      //must have .album.name or .album.uri
       res.json({
         song: response.data.tracks.items,
       })
@@ -61,68 +59,5 @@ router.get('/', (req, res) => {
       }
     })
 });
-// router.get('/', (req, res) => {
-//   let queryValues = Object.values(req.query)
-//   if (queryValues[1] === 'Arthur') {
-//     headers = {
-//       headers: {
-//         'Authorization': 'Basic ' + (Buffer.from(client_id + ':' + client_secret).toString('base64')), //Basic <base64 encoded client_id:client_secret>
-//         'Content-Type': 'application/x-www-form-urlencoded'
-//       }
-//     }
-//     axios.post('https://accounts.spotify.com/api/token', querystring.stringify(data), headers)
-//     .then(response => {
-//       token = response.data.access_token;
-//       returnSearch = queryValues[0]
-//       console.log('🔥', returnSearch)
-//       console.log(token)
-//       headers = {headers: {
-//           'Authorization': 'Bearer ' + token,
-//           'Content-Type': 'application/x-www-form-urlencoded'
-//         }}
-//       })
-//       console.log('https://accounts.spotify.com/api/token', querystring.stringify(data), headers)
-//       console.log(headers)
-//       axios.get(`https://api.spotify.com/v1/search?q=${returnSearch}&type=track&market=US&limit=10&offset=5`, headers)
-//       .then(response => {
-//         console.log('🌎', token)
-//         res.json({
-//           song: response.data.tracks.items,
-//           spotifyToken: token
-//         })
-//       }).catch(err => {
-//         if (err) {
-//           console.error(`🧤WE HAVE AN ERROR IN THE AXIOS Get`, err);
-//         }
-//     }).catch(err => {
-//       if (err) {
-//         console.error(`🐶WE HAVE AN ERROR IN THE AXIOS CALL`, err);
-//       }
-//   })
-//     } else {
-//       console.log('🌎', token)
-//       returnSearch = queryValues[0]
-//       headers ={headers: {
-//         'Authorization': 'Bearer ' + queryValues[1],
-//         'Content-Type': 'application/x-www-form-urlencoded'
-//       }}
-//         axios.get(`https://api.spotify.com/v1/search?q=${returnSearch}&type=track&market=US&limit=10&offset=5` , headers)
-//         .then (response => {
-//           // console.log('🦀', (response.query));
-//           //must have .album.name or .album.uri
-//           res.json({
-//             song: response.data.tracks.items,
-//             spotifyToken: token
-//           })
-//         }).catch(err => {
-//           if (err) {
-//             console.error(`WE HAVE AN ERROR IN THE SECOND AXIOS Get`+ err);
-//           }
-//         }).catch(err => {
-//       if (err) {
-//         console.error(`WE HAVE AN ERROR IN THE AXIOS CALL`+ err);
-//       }
-//     })
-//   }
-// });
+
 module.exports = router;
